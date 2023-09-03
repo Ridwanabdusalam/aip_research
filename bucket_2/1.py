e561ed39bdc724cc6cf215ba3d833e3bedf03384
Ridwan <ridwanabdsalam@gmail.com>
2023-09-03 16:29:42 -0700

    Update 1.py

--- bucket_2/1.py
+++ bucket_2/1.py
@@ -138,3 +138,50 @@ class STwitterForKidsService:
 
     def ExportTweets(self, request: MExportTweetsRequest) -> MExportTweetsResponse:
         pass
+
+
+
+
+
+
+#CLIENT SIDE:
+import grpc
+from child_twitter_pb2 import MCreateTweetRequest, MDeleteTweetRequest, MLikeTweetRequest, MCreateCommentRequest
+from child_twitter_pb2_grpc import STwitterForKidsServiceStub
+
+# Create a gRPC channel to connect to the server
+channel = grpc.insecure_channel('localhost:50051')  # Replace with your server's address
+
+# Create a stub for the TwitterForKidsService
+twitter_service = STwitterForKidsServiceStub(channel)
+
+# Create a tweet
+def create_tweet(parent, tweet_text):
+    tweet = MCreateTweetRequest(parent=parent, tweet=MTweet(text=tweet_text))
+    response = twitter_service.CreateTweet(tweet)
+    print(f"Tweet created: {response.tweet.name}")
+
+# Delete a tweet
+def delete_tweet(tweet_name):
+    tweet = MDeleteTweetRequest(name=tweet_name)
+    twitter_service.DeleteTweet(tweet)
+    print(f"Tweet deleted: {tweet_name}")
+
+# Like a tweet
+def like_tweet(tweet_name):
+    like_request = MLikeTweetRequest(name=tweet_name)
+    twitter_service.LikeTweet(like_request)
+    print(f"Tweet liked: {tweet_name}")
+
+# Reply to a tweet
+def reply_to_tweet(parent_tweet_name, reply_text):
+    reply = MCreateCommentRequest(parent=parent_tweet_name, comment=MComment(text=reply_text))
+    response = twitter_service.CreateComment(reply)
+    print(f"Reply created: {response.name}")
+
+# usage
+if __name__ == "__main__":
+    create_tweet("parent_account", "Hello, Twitter for Kids!")
+    delete_tweet("tweet123")
+    like_tweet("tweet456")
+    reply_to_tweet("tweet789", "That's great!")
